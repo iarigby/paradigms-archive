1d65fbcfb60d9f9911e06394f78850182a0a1d25
iarigby <marvinzem@gmail.com>
2019-11-21 12:39:11 +0400

    update to match handout, + add variatons

--- resources/classwork/23-Concurrency-Examples/tickets.c
+++ resources/classwork/23-Concurrency-Examples/tickets.c
@@ -15,24 +15,31 @@ struct helper {
 
 void * SellTickets(void * data) {
 	struct helper *h = (struct helper*)data;
-	int *numTickets = h->num;
-	int sellerId = h->id;
 
+	int sellerId = h->id;
+	// don't need esmaphore because the addressdoesn't change, only value
+	int *numTickets = h->num;
+	int ticketsSold = 0;
 	while (1) {
 		sem_wait(h->sem);//mutex_lock
-		if (*numTickets == 0)
+		if (*numTickets == 0) {
+			sem_post(h->sem);//mutex_unlock
 			break;
+		}
 		(*numTickets)--;
-		int tmp = *numTickets;
-		printf("%d Seller Sold %d \n", sellerId, tmp);
+		ticketsSold++;
+		printf("%d Seller Sold %d \n", sellerId, *numTickets);
 		sem_post(h->sem);//mutex_unlock
+		// try without sleep
 		usleep(100);
 	}
-	sem_post(h->sem);//mutex_unlock
+	printf("%d noticed all tickets sold! (I sold %d myself) \n",
+		   sellerId, ticketsSold);
 }
 
 int main() {
 	int numSellers = 10;
+	// try 40000 without sleep
 	int numTickets = 40;
 
 	pthread_t t[10];
@@ -47,7 +54,7 @@ int main() {
 		h[i].sem = &sem;
 		pthread_create(t+i, NULL, SellTickets, h+i);
 	}
-
+	printf("here\n");
 	pthread_exit(NULL);
 }
 
