7a16e6feadd5e83308945530745cbc6c6dbce9cf
iarigby <marvinzem@gmail.com>
2019-11-26 11:24:12 +0400

    phil seminar

--- resources/classwork/23-Concurrency-Examples/philosophers.c
+++ resources/classwork/23-Concurrency-Examples/philosophers.c
@@ -8,8 +8,7 @@
 #define PHIL_NUM 10
 
 typedef struct {
-	int id;
-	sem_t * locks;
+//
 } dataT;
 
 size_t getRandNumber(size_t start, size_t end) {
@@ -27,36 +26,11 @@ void Eat(int id) {
 }
 
 void * Philospher(void * data) {
-	dataT * d = (dataT *)data;
-	while (1) {
-		Think(d->id);
-		if (d->id < (d->id + 1)%PHIL_NUM) {
-			sem_wait(d->locks + d->id);
-			sem_wait(d->locks + (d->id + 1)%PHIL_NUM);
-		} else {
-			sem_wait(d->locks + (d->id + 1)%PHIL_NUM);
-			sem_wait(d->locks + d->id);
-		}
-		Eat(d->id);
-		sem_post(d->locks + d->id);
-		sem_post(d->locks + (d->id + 1)%PHIL_NUM);
-	}
+
 }
 
 int main() {
-	dataT * arr = malloc(PHIL_NUM * sizeof(dataT));
-	int i = 0;
-	sem_t * locks = malloc(PHIL_NUM * sizeof(sem_t));
 
-	for (; i<PHIL_NUM; i++) {
-		arr[i].id = i;
-		sem_init(locks + i, 0, 1);
-		arr[i].locks = locks;
-	}
-	pthread_t threads[PHIL_NUM];
-	for (i = 0; i<PHIL_NUM; i++) {
-		pthread_create(threads+i, NULL, Philospher, arr+i);
-	}
 
 	pthread_exit(NULL);
 }
